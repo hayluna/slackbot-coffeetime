f19339ebd094e5a4b6a3a74e916617d639360bbc
명혜은 <hayluna2@gmail.com>
2022-10-13 15:46:06 +0900

    feat: 제훈쌤 추가

--- src/constants/members.js
+++ src/constants/members.js
@@ -1,25 +1,25 @@
 const members = [
-  '강승훈',
-  '김회준',
-  '임한울',
-  '명혜은',
-  '성민화',
-  '조수현',
-  '김태련',
-  '육용수',
-  '김용현',
-  '임민석',
-  '신제훈',
-  'Junseok Oh',
-  '구주승',
-  '노기진',
-  '유시온',
-  '이진혁',
-  '조충근',
+  "강승훈",
+  "김회준",
+  "임한울",
+  "명혜은",
+  "성민화",
+  "조수현",
+  "김태련",
+  "육용수",
+  "김용현",
+  "임민석",
+  "신제훈",
+  "Junseok Oh",
+  "구주승",
+  "노기진",
+  "유시온",
+  "이진혁",
+  "조충근",
 ];
 
 const memberSet = new Set([...members]);
-const excludedMembers = ['coffeetime', '신제훈', '조충근'];
+const excludedMembers = ["coffeetime", "조충근"];
 
 const excludedMemberSet = new Set([...excludedMembers]);
 
