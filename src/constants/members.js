45b36b82dcd26919d8aca95ae4dc68d2ff212249
명혜은 <hayluna2@gmail.com>
2023-03-08 11:00:05 +0900

    fix: 멤버 수정

--- src/constants/members.js
+++ src/constants/members.js
@@ -19,7 +19,7 @@ const members = [
 ];
 
 const memberSet = new Set([...members]);
-const excludedMembers = ["coffeetime", "김하늘", "명혜은", "박종대", "최지예"];
+const excludedMembers = ["coffeetime", "유시온", "명혜은", "박종대"];
 
 const excludedMemberSet = new Set([...excludedMembers]);
 
