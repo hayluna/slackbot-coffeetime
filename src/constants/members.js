ca5a95be1ffce37b1e6f0b2d3b7200ee00776019
명혜은 <hayluna2@gmail.com>
2023-11-06 14:30:12 +0900

    fix: 운교쌤 복귀

--- src/constants/members.js
+++ src/constants/members.js
@@ -19,13 +19,7 @@ const members = [
 ];
 
 const memberSet = new Set([...members]);
-const excludedMembers = [
-  "coffeetime",
-  "Ahram Chang",
-  "정운교",
-  "신규식",
-  "신지원",
-];
+const excludedMembers = ["coffeetime", "Ahram Chang", "신규식"];
 
 const excludedMemberSet = new Set([...excludedMembers]);
 
